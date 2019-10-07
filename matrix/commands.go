9b1524642e6a01f1bdda37cf4eab1a7c48bf029a
Silke Hofstra <silke@slxh.eu>
2019-10-07 18:41:26 +0200

    Improve duration formatting for ping command

--- matrix/commands.go
+++ matrix/commands.go
@@ -1,6 +1,7 @@
 package matrix
 
 import (
+	"fmt"
 	"html"
 	"strings"
 	"text/template"
@@ -10,15 +11,18 @@ import (
 )
 
 const (
-	pingCommandResponseBody          = "{{.Sender}}: Pong! (ping {{.Message}} {{.Duration}} to arrive)"
+	pingCommandResponseBody          = "{{.Sender}}: Pong! (ping {{.Message}} {{formatDuration .Duration}} to arrive)"
 	pingCommandResponseFormattedBody = "<a href='https://matrix.to/#/{{.Sender}}'>{{.Sender}}</a>: Pong! " +
-		"(<a href='https://matrix.to/#/{{.RoomID}}/{{.ID}}'>ping</a> {{.Message}} {{.Duration}} to arrive)"
+		"(<a href='https://matrix.to/#/{{.RoomID}}/{{.ID}}'>ping</a> {{.Message}} {{formatDuration .Duration}} to arrive)"
 )
 
-var (
-	pingTextTemplate = template.Must(template.New("ping").Parse(pingCommandResponseBody))
-	pingHTMLTemplate = template.Must(template.New("ping").Parse(pingCommandResponseFormattedBody))
-)
+var pingTemplate *template.Template
+
+func init() {
+	pingTemplate = template.New("").Funcs(template.FuncMap{"formatDuration": formatDuration})
+	pingTemplate = template.Must(pingTemplate.New("text").Parse(pingCommandResponseBody))
+	pingTemplate = template.Must(pingTemplate.New("html").Parse(pingCommandResponseFormattedBody))
+}
 
 type pingEvent struct {
 	*matrix.Event
@@ -57,8 +61,8 @@ func (c *Client) pingHandler(e *matrix.Event, received time.Time) error {
 
 	// Create response
 	var plain, formatted strings.Builder
-	_ = pingTextTemplate.Execute(&plain, event)
-	_ = pingHTMLTemplate.Execute(&formatted, event)
+	_ = pingTemplate.ExecuteTemplate(&plain, "text", event)
+	_ = pingTemplate.ExecuteTemplate(&formatted, "html", event)
 
 	// Create response
 	response := &pingMessage{
@@ -82,4 +86,15 @@ func (c *Client) pingHandler(e *matrix.Event, received time.Time) error {
 func getArgs(e *matrix.Event, n int) []string {
 	body, _ := e.Body()
 	return strings.SplitN(body, " ", n+1)[1:]
+}
+
+func formatDuration(d time.Duration) string {
+	switch {
+	case d < 10 * time.Second:
+		return fmt.Sprintf("%d ms", d.Milliseconds())
+	case d < 1 * time.Minute:
+		return fmt.Sprintf("%.1f second", d.Seconds())
+	default:
+		return fmt.Sprintf("%6s", d.Truncate(time.Second))
+	}
 }
